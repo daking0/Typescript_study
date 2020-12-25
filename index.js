a45d85a965b8bc912b84159dcc3ec46718db5bf7
daking0 <angk226@gmail.com>
2020-12-25 22:58:46 +0900

    interface

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sayHi = function (name, age, gender) {
-    return "Hello " + name + ", you are a " + gender;
-};
-console.log(sayHi("Hello", 24, "male"));
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,7 +0,0 @@
-const sayHi = (name:string, age:number, gender:string) : string => {
-    return `Hello ${name}, you are a ${gender}`;
-};
-
-console.log(sayHi("Hello",24,"male"));
-
-export {};
