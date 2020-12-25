1ab69ffa731d592fbf20365c8dc4ed8af4b7f5e7
daking0 <angk226@gmail.com>
2020-12-25 23:04:32 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
-interface Human{
-    name : string,
-    age:number,
-    gender:string
+// interface Human{
+//     name : string,
+//     age:number,
+//     gender:string
+// } 
+//인터페이스는 js로 변환되지 않는다. => index.js에 나타나지 않음.
+class Human {
+    public name :string;
+    public age : number;
+    public gender : string;
+    constructor(name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
-const person = {
-    name : "nicolas",
-    age:22,
-    gender : "male"
-}
-const sayHi = (person:Human) : string => {
+const dakyung = new Human("dakyung",27,"female");
+
+// const person = {
+//     name : "nicolas",
+//     age:22,
+//     gender : "male"
+// }
+const sayHi = (person) : string => {
     return `Hello ${person.name},your age is ${person.age} you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(dakyung));
 
 export {};
