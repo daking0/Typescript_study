59d43cc3277d3fea3d71105c7920a26db0ec4c1d
daking0 <angk226@gmail.com>
2020-12-25 20:48:03 +0900

    tsc-watch 설치전까지

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sayHi = function (name, age, gender) {
+    return "Hello " + name + ", you are a " + gender;
+};
+console.log(sayHi("Hello", 24, "male"));
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("test")
+const sayHi = (name:string, age:number, gender:string) : string => {
+    return `Hello ${name}, you are a ${gender}`;
+};
+
+console.log(sayHi("Hello",24,"male"));
+
+export {};
